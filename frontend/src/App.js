9b8493b53d3c920583c4240c9889ee016d12858b
chanukasenevirathne24@gmail.com <chanukasenevirathne24@gmail.com>
2025-04-23 13:28:04 +0530

    VF 2.0.2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import axios from 'axios';
 
-
 // Finance Manager Components
 import AddEmployee from "./Components/AddEmployee";
 import AllEmployees from "./Components/AllEmployee";
