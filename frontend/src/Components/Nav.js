8e6d375da19c5c6f6a79ee1fd3d9bc3e7f8f35cd
Chanuka Navod <chanukanavod17177@gmail.com>
2025-04-27 22:32:44 +0530

    VS 3.4.1

--- frontend/src/Components/Nav.js
+++ frontend/src/Components/Nav.js
@@ -43,12 +43,6 @@ function Nav() {
               <li className="nav-item">
                 <Link className="nav-link text-white" to="/supplier">Suppliers</Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/add-feedback">Feedback Form</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/faq">FAQ</Link>
-              </li>
             </ul>
 
             <ul className="navbar-nav ml-auto">
